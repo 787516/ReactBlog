d5f87f0547d66adb3a0d6c73808ae9820fbad1e3
VaibhavAPatil <vaibhavpatil.vap@gmail.com>
2024-07-05 00:50:42 +0530

    Footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
             <footer className="bg-white dark:bg-zinc-800">
                 <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                     <span className="text-sm text-zinc-500 sm:text-center dark:text-zinc-400">
-                        © 2023{" "}
-                        <a href="https://flowbite.com/" className="hover:underline">
-                            Flowbite™
+                        © 2024{" "}
+                        <a href="/" className="hover:underline">
+                            News
                         </a>
                         . All Rights Reserved.
                     </span>
